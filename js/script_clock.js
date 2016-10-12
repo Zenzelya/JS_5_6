5e9e7dee8f2e1547e160356aab37ef3c9a9d6c4c
Andrey <Andrey>
2016-10-12 18:50:46 +0300

    finish clock

--- js/script_clock.js
+++ js/script_clock.js
@@ -1,28 +1,56 @@
 var mSecods = 0,
-	secondsX = 0, 
-	secondsY = 0,
-	minetsX = 0,
-	minetsY = 0,
-	hourX = 0,
-	hourY = 0;
-
-var timer = document.getElementById("clock");
-console.log(timer);
-console.log(hourX + "" + hourY + " : " + minetsX + "" + minetsY + " : " +  secondsX + "" + secondsY + " : " + mSecods);
-//document.getElementById('clock').innerHTML = hourX + "" + hourY + " : " + minetsX + "" + minetsY + " : " +  secondsX + "" + secondsY + " : " + mSecods;
-// (hourX, hourY, + " : " + minetsX, minetsY + " : " +  secondsX,  + " : " + mSecods);
- 
+	switcher = 0;
 
 
-function setMoliSeconds () {
-	this.mSecods = 0;
-	if (mSecods == 10) {
-		setSeconds();
-		mSecods = 0;
+function reset() {
+	switcher = 0;
+	mSecods = 0;
+	document.getElementById('clock').innerHTML = "00:00:00:0";
+	document.getElementById('start').innerHTML = "Start button";
+	
+}
+
+function startOrePause () {
+	if (switcher == 0){
+		switcher = 1;
+		clock();
+		document.getElementById('start').innerHTML = "Pause button";
+	} else {
+		switcher = 0;
+		document.getElementById('start').innerHTML = "Continue button";
 	}
-	console.log(document.getElementById('clock').innerHTML = hourX + "" + hourY + " : " + minetsX + "" + minetsY + " : " +  secondsX + "" + secondsY + " : " + mSecods);
-	mSecods = mSecods + 1;
+
+}
+
+function clock () {
+	
+	mSecods++;
+	var sec = Math.floor(mSecods * 4 / 1000 % 60);
+	var min = Math.floor(mSecods* 4 / 1000 /60) %60;
+	var hour = Math.floor(mSecods *4 / (1000 * 60 * 60)% 24);
+	var tenth = Math.floor(mSecods* 4 % 1000);
+	
+	if(sec < 10){
+		sec = "0" + sec;
+	}
+	if(min < 10){
+		min = "0" + min;
+	}
+	
+	if(hour < 10) {
+		hour = "0" + hour;
+	}
+	
+	document.getElementById('clock').innerHTML = hour + ' : ' + min + ' : ' + sec + ' : ' + tenth;
+	// document.getElementById('tenth').innerHTML = tenth;
+	if(switcher == 1){
+		setTimeout(clock, 1);
+	}
+	
 }
 
  
- setInterval(setMoliSeconds, 100);
+
+document.getElementById('start').addEventListener('click', startOrePause);
+
+document.getElementById('clear').addEventListener('click', reset);
